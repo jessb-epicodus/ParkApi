26b9af44d4110581327edb211f34d7f7090d4167
jb <jessb.epicodus@gmail.com>
2022-04-01 14:52:20 -0700

    WIP: versioning not building

--- USParkAPI/Controllers/ParksController.cs
+++ USParkAPI/Controllers/ParksController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using USParkAPI.Models;
 
 namespace USParkAPI.Controllers {
+  [ApiVersion("1.0")]
   [Route("api/[controller]")]
   [ApiController]
   public class ParksController : ControllerBase {
@@ -16,6 +17,97 @@ namespace USParkAPI.Controllers {
       _db = db;
     }
 
+    // GET api/parks?endpoint  (read)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get(string name, string city, string state, string managedBy, string activities, string amenities, bool ada) {
+      var query = _db.Parks.AsQueryable();
+      if (name != null) {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (city != null) {
+        query = query.Where(entry => entry.City == city);
+      }
+      if (state != null) {
+        query = query.Where(entry => entry.State == state);
+      }
+      if (managedBy != null) {
+        query = query.Where(entry => entry.ManagedBy == managedBy);
+      }
+      if (activities != null) {
+        query = query.Where(entry => entry.Activities == activities);
+      }
+      if (amenities != null) {
+        query = query.Where(entry => entry.Amenities == amenities);
+      }
+      if (ada == true) {
+        query = query.Where(entry => entry.ADA == ada);
+      }
+      return await query.ToListAsync();
+    }
+    
+    // GET: api/parks/id  (read)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id) {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null) {
+        return NotFound();
+      }
+      return park;
+    }
+    
+    // POST api/parks  (create)
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park) {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);; // returns Park object; status: "201, Created"
+    }
+
+    // PUT: api/parks/${id}  (update)
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park) {
+      if (id != park.ParkId) {
+        return BadRequest();
+      }
+      _db.Entry(park).State = EntityState.Modified;
+      try {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException) {
+        if (!ParkExists(id)) {
+          return NotFound();
+        } else {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool ParkExists(int id){
+      return _db.Parks.Any(e => e.ParkId == id);
+    }
+
+    // DELETE: api/parks  (delete)
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id) {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null) {
+        return NotFound();
+      }
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+  }
+
+  [ApiVersion("2.0")]
+  [Route("api/[controller]")] // /api/values?api-version=2.0
+  [ApiController]
+  public class ParksV2Controller : ControllerBase {
+    private readonly USParkAPIContext _db;
+
+    public ParksV2Controller(USParkAPIContext db) {
+      _db = db;
+    }
     // GET api/parks?endpoint  (read)
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Park>>> Get(string name, string city, string state, string managedBy, string activities, string amenities, bool ada) {
--- USParkAPI/Startup.cs
+++ USParkAPI/Startup.cs
@@ -19,6 +19,11 @@ namespace USParkAPI
             services.AddDbContext<USParkAPIContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddApiVersioning(o => {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
